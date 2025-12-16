61205a8f02657883e6d576eef471c7874fb8a86a
AyoubKhaoua <mr.ayoubkhaoua@example.com>
2025-12-17 00:21:13 +0100

    Add database schema with users, themes, and notes tables

---
+++ sql/schema.sql
@@ -0,0 +1,37 @@
+-- Active: 1764683361796@@127.0.0.1@3306@digitalgarden
+CREATE DATABASE DigitalGarden;
+
+use DigitalGarden;
+-----create tables
+--1-tabel users
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+alter TABLE users add COLUMN email VARCHAR(100) not NULL;
+
+alter table users add UNIQUE (email);
+--2-table themes
+CREATE TABLE themes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(150) NOT NULL,
+    color VARCHAR(7) DEFAULT '#ffffff',
+    nbrnotes int DEFAULT 0,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+--2-tbale notes
+CREATE TABLE notes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    theme_id INT NOT NULL,
+    title VARCHAR(255),
+    content TEXT,
+    importance ENUM('1', '2', '3', '4', '5'),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (theme_id) REFERENCES themes (id) ON DELETE CASCADE
+);
+-----
+SELECT * from users;
